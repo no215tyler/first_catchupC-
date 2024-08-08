1700ecd7bbab3c995e795526e24dddb49816a7d0
no215tyler <no.215.tyler@gmail.com>
2024-08-09 00:15:53 +0900

    GPT問題集C#01

--- first_catchup/Program.cs
+++ first_catchup/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+class Program
+{
+    static void Main()
+    {
+        int num = 3;
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else
+        {
+            Console.WriteLine("Odd");
+        }
+    }
+}
